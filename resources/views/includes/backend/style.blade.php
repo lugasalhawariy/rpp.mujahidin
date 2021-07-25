ea2502e351dbeda31b79007e56b45bf09f81b7d9
lugasalhawariy <lugasalhawariy@gmail.com>
2021-07-26 01:16:56 +0700

    pararusing

--- resources/views/includes/backend/style.blade.php
+++ resources/views/includes/backend/style.blade.php
@@ -1,7 +1,6 @@
 <!-- VENDOR CSS -->
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-{{-- <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> --}}
-{{-- <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/font-awesome/css/font-awesome.min.css') }}"> --}}
+<link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/font-awesome/css/font-awesome.min.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/linearicons/style.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/chartist/css/chartist-custom.css') }}">
 <!-- MAIN CSS -->
@@ -15,4 +14,3 @@
 <link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('backend/assets/img/logo.png') }}">
 <!-- CSS only -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
