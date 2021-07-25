e037ec6d37b29543bdaa3ae809a8e291cdfe7a46
lugasalhawariy <lugasalhawariy@gmail.com>
2021-07-26 02:31:19 +0700

    fonticon

--- resources/views/includes/backend/style.blade.php
+++ resources/views/includes/backend/style.blade.php
@@ -1,7 +1,7 @@
 <!-- VENDOR CSS -->
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/bootstrap/css/bootstrap.min.css') }}">
 {{-- <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/font-awesome/css/font-awesome.min.css') }}"> --}}
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+{{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous"> --}}
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/linearicons/style.css') }}">
 <link rel="stylesheet" href="{{ secure_asset('backend/assets/vendor/chartist/css/chartist-custom.css') }}">
 <!-- MAIN CSS -->
@@ -15,3 +15,4 @@
 <link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('backend/assets/img/logo.png') }}">
 <!-- CSS only -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
